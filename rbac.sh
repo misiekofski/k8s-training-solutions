85c29d1b7c1dc1fb993b81e0f8584c752e4427a2
Michal Dobrzycki <dobrzycki@gmail.com>
2021-05-26 12:13:40 +0200

    First commit

---
+++ rbac.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+USER=$1
+TEAM=testers
+# Uncomment the following lines only when openssl is not available at you os.
+#apt-get update
+#apt-get install -y openssl
+mkdir certs
+cd certs
+
+openssl genrsa -out $USER.pem 2048
+openssl req -new -key $USER.pem -out $USER-csr.pem -subj "/CN=$USER/O=$TEAM/"
+openssl x509 -req -in $USER-csr.pem -CA ~/.minikube/ca.crt -CAkey ~/.minikube/ca.key -CAcreateserial -out $USER.crt -days 365
